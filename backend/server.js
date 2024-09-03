8bb6cb1ce7a8be1fb6616af8564558e1dd6dcb4c
juanmarcoseb <juanmarcoseb@users.noreply.github.com>
2024-09-03 21:52:25 +0000

    deploy: 6c454c4a130f7df8c648be2a747fffbb6cfdfe76

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ const uri = process.env.MONGODB_URI;
 
 // Conectar a MongoDB
 mongoose.connect(uri, {
+    useNewUrlParser: true, // Añadido para compatibilidad con versiones más nuevas de MongoDB
+    useUnifiedTopology: true, // Añadido para manejar la topología de conexiones de manera óptima
     serverSelectionTimeoutMS: 50000 // 50 segundos
 })
     .then(() => console.log('Conectado a MongoDB'))
@@ -26,6 +28,11 @@ mongoose.connect(uri, {
 
 // Usar las rutas de reportes
 app.use('/reportes', reportRoutes);
+
+// Ruta de prueba para la raíz
+app.get('/', (req, res) => {
+    res.send('Bienvenido al sistema de reportes');
+});
 
 // Iniciar el servidor
 app.listen(PORT, () => {
